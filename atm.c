8fe82daf03cbfd204add71e497a1435d1cc38eae
jyotipal2001 <77713523+jyotipal2001@users.noreply.github.com>
2021-05-06 15:42:28 +0530

    Add files via upload

---
+++ atm.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+void main(){
+	float x,y;
+	char ch;
+	printf("enter initial amount\n");
+	scanf("%f",&x);
+	printf("enter\n c for credit \n d for debit\n b for balance\n");
+	scanf("\n%c",&ch);
+	switch(ch){
+		case 'c':
+			printf("enter credit amount\n");
+			scanf("%f",&y);
+			x=x+y;
+			printf("new amount =%f",x);
+			break;
+			case 'd':
+				printf("enter debit amount\n");
+				scanf("\n%c",&y);
+				if(x>=y){
+					x=x-y;
+					printf("new amount=%f",x);
+				}
+				else{
+					printf("insufficient amount in your account");
+				}
+					break;
+					case 'b':
+						printf("amount in your account=%f",x);
+						break;
+						default:
+							printf("choose correct option for your operation");
+	}
+}
